585ac3cb75d1dd948592100e9191f4c0a5ca88b4
thuongtruong1009 <ititiu19228@student.hcmiu.edu.vn>
2024-02-21 16:58:11 +0700

    chore: update somethings

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
+# Charts Visualization
+
 [Demo](https://data-visualization-vip.vercel.app/)
+
+### Description
+
+This project is a data visualization project using the chart.js library. The project is a simple web application that allows the user to visualize data in a chart. The user can select the type of chart, the data, and the labels. The user can also customize the chart by changing the color of the chart and the labels.
+
+### Technologies
+
+- Nextjs
+- Chart.js
+- D3
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,41 +1,23 @@
-import Head from "next/head";
-
 import Heatmap from "@/components/Heatmap";
 
-import styles from "@/styles/Home.module.css";
-import Plot from "@/components/Plot";
+import BarChart from "@/components/Bar";
+import BubbleChart from "@/components/Bubble";
+import CrazyChart from "@/components/Crazy";
+import DoughnutChart from "@/components/Doughnut";
+import HorizontalBarChart from "@/components/Horinzontal";
 import LineChart from "@/components/Line";
 import PieChart from "@/components/Pie";
-import RadarChart from "@/components/Rada";
+import Plot from "@/components/Plot";
 import PolarChart from "@/components/Polar";
+import RadarChart from "@/components/Rada";
 import ScatterChart from "@/components/Scatter";
-import HorizontalBarChart from "@/components/Horinzontal";
-import DoughnutChart from "@/components/Doughnut";
-import CrazyChart from "@/components/Crazy";
-import BubbleChart from "@/components/Bubble";
-import BarChart from "@/components/Bar";
+import styles from "@/styles/Home.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.info}>
-        <ul>
-          <h3>Team members</h3>
-          <li>Trần Nguyễn Thương Trường - ITITIU19228</li>
-          <li>Trần Thanh Sơn - ITITIU19201</li>
-          <li>Nguyễn Trung Trực- ITITIU19225</li>
-          <li>Nguyễn Thanh Thanh Tùng - ITITIU19236</li>
-          <li>Phạm Duy Thịnh - ITITIU19215</li>
-          <li>Lê Quốc Phong - ITITIU19181</li>
-          <li>Trần Cẩm Vinh - ITITIU19244</li>
-        </ul>
-        <ul>
-          <h3>Information</h3>
-          <li>Subject: Data Science & Visualization</li>
-          <li>Teacher: Mai Hoàng Bảo Ân</li>
-          <li>Title: Final project 7/1/2023</li>
-          <li>Technologies: Nextjs + D3 + Chartjs </li>
-        </ul>
+        <h1>Charts Visualization</h1>
       </div>
 
       <div className={styles.heatmapContainer}>
